fe6986f9137c1a0183072030f56cd7fe12e8ab1c
kaydenmc77 <kayden-mcmanaway@live.nmit.ac.nz>
2024-05-02 14:05:10 +1200

    kayden function

--- functions.js
+++ functions.js
@@ -1,3 +1,24 @@
-function splitBunches(){
-    
+// An array of objects, make a new object relevant to the quantity
+let bunches = ([
+    { name: "currants", quantity: 1 },
+    { name: "grapes", quantity: 2 },
+    { name: "bananas", quantity: 2 }
+  ])
+
+function splitBunches(fruitArray){
+    let result = []
+    for(let i = 0; i<fruitArray.length; i++) {
+        let amount = fruitArray[i].quantity
+        let originalName = fruitArray[i].name
+        for(let x = 0; x<amount; x++) {
+            const fruit = {
+                name: originalName,
+                quantity: 1
+            }
+            result.push(fruit)
+        }
+    }
+    return result
 }
+
+console.log(splitBunches(bunches))
