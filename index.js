5f05e734669b1253cfef792adf8aebbb3c35b719
Nathaniel Geiger <Nathaniel-Geiger@live.nmit.ac.nz>
2024-05-02 13:52:30 +1200

    working on task 1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-console.log()
+function splitBunches([{name: `currants`, quantity: 1}, {name: "grapes", quantity: 2}, {name: "Bananas", quantity: 2}])
 
-for (let quantity: = i; i > 0; i--;)
+for (let name: = i; i > 0; i--;)
+for (let quantity: = j; j > 0; j--;)
+console.log()
