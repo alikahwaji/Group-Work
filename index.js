bcf6978cb1d97993faafe32602b295366c8c2ad4
jamie-noel <jamie-ashton@live.nmit.ac.nz>
2024-05-02 13:35:20 +1200

    adding file

---
+++ index.js
@@ -0,0 +1,3 @@
+array. splitBunches([ { name: "currants", quantity: 1 }, { name: "grapes", quantity: 2 }, { name: "bananas", quantity: 2 } ]) 
+➞
+ [ { name: "currants", quantity: 1}, { name: "grapes", quantity: 1 }, { name: "grapes", quantity: 1 }, { name: "bananas", quantity: 1 }, { name: "bananas", quantity: 1 } ]
