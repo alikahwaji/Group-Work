9b60110037b40a8b1afc01ca7d2867b0357fd4f6
William Busbridge <william-busbridge@live.nmit.ac.nz>
2024-05-02 14:12:09 +1200

    will-test file

---
+++ will-test.js
@@ -0,0 +1,12 @@
+let fruit = [
+    {
+        name: "bananas",
+        quantity: 2
+    },
+    {
+        name: "apples",
+        quantity: 1
+    }
+]
+
+const seperateObj = () => fruit.ForEach(fruit.quantity > 1)
