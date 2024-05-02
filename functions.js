412fdb5d0deacfa80fc3654d82b4e0ed4da2d177
Erin Colane <erin-colane@live.nmit.ac.nz>
2024-05-02 14:02:02 +1200

    added first link and example array

--- functions.js
+++ functions.js
@@ -1,3 +1,11 @@
-function splitBunches(){
-    
+function splitBunches(arr){
+
 }
+
+
+let fruitArray= [ 
+        { name: "currants", quantity: 1 },
+        { name: "grapes", quantity: 2 },
+        { name: "bananas", quantity: 2 }
+]
+// https://stackoverflow.com/questions/11922383/how-can-i-access-and-process-nested-objects-arrays-or-json
