4a76c442d6ac04ac7d87cc585ff73b86112feb5d
kaydenmc77 <kayden-mcmanaway@live.nmit.ac.nz>
2024-05-02 14:17:16 +1200

    Task Two

--- functions.js
+++ functions.js
@@ -22,14 +22,3 @@ function kaydenSplitBunches(fruitArray){
 }
 
 console.log(kaydenSplitBunches(bunches))
-function splitBunches(arr){
-
-}
-
-
-let fruitArray= [ 
-        { name: "currants", quantity: 1 },
-        { name: "grapes", quantity: 2 },
-        { name: "bananas", quantity: 2 }
-]
-// https://stackoverflow.com/questions/11922383/how-can-i-access-and-process-nested-objects-arrays-or-json
